026af0a21959cb12148d9063d0c00cec85c56867
Ravenelco <jesus.santana.ravenelco@gmail.com>
2022-11-24 14:59:42 -0600

    adc practica

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(void)
     //printString("3");
     while(1)
     {
+        PWM0
         ADC0_InSeq2(Result); //llamada a la conversion por procesador
         valor=(float)(((Result[0]))*3.3)/4096;
         valor1=(float)(((Result[1]))*3.3)/4096;
