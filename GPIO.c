c141c409ebe3b97527fbe435aa06781dfbc3f251
pgomez04 <pgomez04@alumnos.uaq.mx>
2023-01-26 12:31:48 -0600

    configuracion gpio

--- GPIO.c
+++ GPIO.c
@@ -1,25 +1,29 @@
 #include "lib/include.h"
 extern void Configurar_GPIO(void)
-//configuracion gpio
 {
     //Paso 1 Enceder el periferico Run Clock Gate Control GPIO
-    SYSCTL->RCGCGPIO |= (1<<5);
+    SYSCTL->RCGCGPIO |= (1<<5);//enable GPIOF,GPION,GPIOJ
+    //Paso 2
     GPIOF->LOCK = 0x4C4F434B;
-    GPIOF->CR = 0x1F;           // allow changes to PF4-0
-    GPIOF->AMSEL = 0x00;        // 3) disable analog on PF
-    GPIOF->PCTL = 0x00000000;   // 4) PCTL GPIO on PF4-0
-    GPIOF->DIR = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-    GPIOF->AFSEL = 0x00;        // 6) disable alt funct on PF7-0
-    GPIOF->PUR = 0x11;          // enable pull-up on PF0 and PF4
-    GPIOF->DEN = 0x1F;          // 7) enable digital I/O on PF4-0
+    GPIOF->CR= 0x1F;
+    GPIOF->AMSEL = 0x00;
+    //Salidas y entradas
+    GPIOF->DIR |= 0x0E;
+    //Paso 3
+    GPIOF->AFSEL |= 0x00;
+
+    GPIOF->PCTL|= 0x00;
+    GPIOF->PUR |= 0x11;
+    GPIOF->DEN |= 0x1F;
 }
 
 extern void Delay(void)
 {
-  unsigned long volatile time;
-  time = 1600000;
-  while(time)
-  {
-		time--;
-  }
+    unsigned long volatile time;
+    time = 1600000;
+    while(time)
+    {
+        time--;
+    }
 }
+
